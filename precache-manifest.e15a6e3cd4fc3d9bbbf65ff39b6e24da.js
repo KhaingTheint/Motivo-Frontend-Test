269afaf7ecb1963d6dd4491534172b2b5b1e108c
Khin Aye Mu <khinayemu@aceplussolutions.com>
2018-12-27 15:29:29 +0630

    Updates

---
+++ precache-manifest.e15a6e3cd4fc3d9bbbf65ff39b6e24da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ece25337b6897b94c96f",
+    "url": "/frontend-test/static/js/runtime~main.ece25337.js"
+  },
+  {
+    "revision": "bdfa4aba315cd7afb39c",
+    "url": "/frontend-test/static/js/main.bdfa4aba.chunk.js"
+  },
+  {
+    "revision": "00c31166f8634460c282",
+    "url": "/frontend-test/static/js/1.00c31166.chunk.js"
+  },
+  {
+    "revision": "bdfa4aba315cd7afb39c",
+    "url": "/frontend-test/static/css/main.6000d848.chunk.css"
+  },
+  {
+    "revision": "2b4a4c90660df423ba2828ba1e62166b",
+    "url": "/frontend-test/index.html"
+  }
+];
